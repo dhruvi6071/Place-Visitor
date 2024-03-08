f9b646adc22a5cfeddfce072667d3d058f2880c8
dhruvi6071 <dhruvi6071@gmail.com>
2024-03-08 15:49:34 +0530

    send-http-request-using-fetch

--- place-visitor/place-visitor/src/components/AvailablePlaces.jsx
+++ place-visitor/place-visitor/src/components/AvailablePlaces.jsx
@@ -1,10 +1,29 @@
-import Places from './Places.jsx';
+import { useState } from "react";
+
+import Places from "./Places.jsx";
+import { response } from "express";
+
+// In React use of localStorage to store and retrieve data in the browser. Use of localStorage.getItem() to retrieve stored data:
+
+// async await is not implemented for react components.
+
+const places = localStorage.getItem("places");
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [AvailablePlaces, setAvailablePlaces] = useState([]);
+  // fetch is provided directly by browser and it is used to send http request on site.
+
+  fetch("http://localhost:3000/places").then((response) => {
+    return response.json();
+  })
+  .then((resData) => {
+    setAvailablePlaces( resData.places);
+  });
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={AvailablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
