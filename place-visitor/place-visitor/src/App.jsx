040f667d4ab5f513d06ac3edaf36d961e7471fb4
dhruvi6071 <dhruvi6071@gmail.com>
2024-03-10 15:08:37 +0530

    delete-http-request-from-data

--- place-visitor/place-visitor/src/App.jsx
+++ place-visitor/place-visitor/src/App.jsx
@@ -47,13 +47,27 @@ function App() {
     }
   }
 
-  const handleRemovePlace = useCallback(async function handleRemovePlace() {
-    setUserPlaces((prevPickedPlaces) =>
-      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
-    );
+  const handleRemovePlace = useCallback(
+    async function handleRemovePlace() {
+      setUserPlaces((prevPickedPlaces) =>
+        prevPickedPlaces.filter(
+          (place) => place.id !== selectedPlace.current.id
+        )
+      );
 
-    setModalIsOpen(false);
-  }, []);
+      try {
+        await updateUserPlaces(
+          userPlaces.filter((place) => place.id !== selectedPlace.current.id)
+        );
+      } catch (error) {
+        setUserPlaces(userPlaces);
+        errorUpdating({ message: error.message || "Failed to update" });
+      }
+
+      setModalIsOpen(false);
+    },
+    [userPlaces]
+  );
 
   function handleError() {
     setErrorUpdating(null);
--- place-visitor/place-visitor/src/http.js
+++ place-visitor/place-visitor/src/http.js
@@ -1,28 +1,27 @@
 export async function fetchAvailablePlaces() {
-    const response = await fetch("http://localhost:3000/places");
-    const resData = await response.json();
+  const response = await fetch("http://localhost:3000/places");
+  const resData = await response.json();
 
-    if (!response.ok) {
-      // const error = new Error("Failed to fetch");
-      // throw error;
-      throw new Error("Failed to fetch");
-    }
-    return resData.places;
+  if (!response.ok) {
+    // const error = new Error("Failed to fetch");
+    // throw error;
+    throw new Error("Failed to fetch");
+  }
+  return resData.places;
 }
 
 export async function updateUserPlaces(places) {
-  const response = await fetch('http://localhost:3000/user-places', {
-    method:'PUT',
-    body: JSON.stringify({places}),
+  const response = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    body: JSON.stringify({ places }),
     headers: {
-      'Content-Type' : 'application/json'
+      "Content-Type": "application/json",
     },
   });
 
   const resData = await response.json();
-  if(!response.ok) {
-    throw new Error('Failed to update user');
-
+  if (!response.ok) {
+    throw new Error("Failed to update user");
   }
 
   return resData.message;
