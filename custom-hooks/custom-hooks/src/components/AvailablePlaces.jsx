989957767dd3bd770ccf7b7064e04698b6dd2661
dhruvi6071 <dhruvi6071@gmail.com>
2024-03-12 14:13:51 +0530

    using-customHook-in-multiple-component

--- custom-hooks/custom-hooks/src/components/AvailablePlaces.jsx
+++ custom-hooks/custom-hooks/src/components/AvailablePlaces.jsx
@@ -4,11 +4,23 @@ import Places from './Places.jsx';
 import Error from './Error.jsx';
 import { sortPlacesByDistance } from '../loc.js';
 import { fetchAvailablePlaces } from '../http.js';
+import { useFetch } from '../hooks/useFetch.js';
+// navigator.geolocation.getCurrentPosition((position) => {
+//   const sortedPlaces = sortPlacesByDistance(
+//     places,
+//     position.coords.latitude,
+//     position.coords.longitude
+//   );
+//   setAvailablePlaces(sortedPlaces);
+//   setIsFetching(false);
+// });
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [isFetching, setIsFetching] = useState(false);
-  const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [error, setError] = useState();
+  // const [isFetching, setIsFetching] = useState(false);
+  // const [availablePlaces, setAvailablePlaces] = useState([]);
+  // const [error, setError] = useState();
+
+  const {isFetching, error, fetchData: availablePlaces, setFetchData: setAvailablePlaces} = useFetch(fetchAvailablePlaces, []);
 
   useEffect(() => {
     async function fetchPlaces() {
